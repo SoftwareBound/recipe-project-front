ba3f6288006890cbd3938cda1c59779099d53dbd
SoftwareBound <benzvi.ron@gmail.com>
2023-08-01 14:13:55 +0300

    sdsad

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,19 +4,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PageRecipeComponent } from './recipe-page/page-recipe/page-recipe.component';
 import { CommonModule } from '@angular/common';
-import { MainRecipePageComponent } from './main-recipe-page/main-recipe-page/main-recipe-page.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
-  declarations: [AppComponent, PageRecipeComponent, MainRecipePageComponent],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    CommonModule,
-    BrowserAnimationsModule,
-    MatIconModule,
-  ],
+  declarations: [AppComponent, PageRecipeComponent],
+  imports: [BrowserModule, AppRoutingModule, CommonModule],
   providers: [],
   bootstrap: [AppComponent],
 })
